8a89b85b8d3d7ec410e69d3d35dec48d88768176
Abdulrahman <abdlrahman@eramint.com>
2022-11-15 23:43:54 +0200

    added style for details page

--- src/Components/HistoricalTable.js
+++ src/Components/HistoricalTable.js
@@ -1,19 +1,25 @@
 import React from "react";
 
-const HistoricalTable = () => {
+const HistoricalTable = (props) => {
+  const historicalArray = props.historicalData;
+
+  console.log(historicalArray);
+
   return (
     <div>
-      <h3> Historical value Of USD</h3>
-      <ul>
-        <li>last Day</li>
-        <li>last Month</li>
-        <li>last Year</li>
-      </ul>
-      <ul>
-        <li>12</li>
-        <li>11</li>
-        <li>6</li>
-      </ul>
+      <h3 className="text-center"> Historical value Of USD</h3>
+      <div className="grid-container">
+        <ul>
+          <li>last Day</li>
+          <li>last Month</li>
+          <li>last Year</li>
+        </ul>
+        <ul>
+          <li>12</li>
+          <li>11</li>
+          <li>6</li>
+        </ul>
+      </div>
     </div>
   );
 };
